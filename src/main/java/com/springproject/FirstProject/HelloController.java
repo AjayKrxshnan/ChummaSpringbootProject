8ca6c4621711889605ed7bd4bca3c083b8237da0
Rengarajan <arenga746@apac.comcast.com>
2026-03-03 12:50:08 +0530

    greet user with name added

--- src/main/java/com/springproject/FirstProject/HelloController.java
+++ src/main/java/com/springproject/FirstProject/HelloController.java
@@ -1,16 +1,40 @@
 package com.springproject.FirstProject;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+class User{
+    String name;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+
 
 @RestController
 public class HelloController {
 
 
+
+
+
+
     @GetMapping("/hello")
-    private String displayHello(){
+    public String displayHello(){
         return "Hello world";
     }
+    // User user = new User()
+    //
 
+    @PostMapping("/hello")
+    public String greetUser(@RequestBody User user){
+        return  "Hello "+user.getName()+" !";
+
+    }
 }
