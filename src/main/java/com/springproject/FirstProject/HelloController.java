d31e3e915796cdc9371fca8f87a6fb9bba997aef
Rengarajan <arenga746@apac.comcast.com>
2026-03-03 11:50:31 +0530

    HelloController api for helloworld

--- src/main/java/com/springproject/FirstProject/HelloController.java
+++ src/main/java/com/springproject/FirstProject/HelloController.java
@@ -1,4 +1,16 @@
 package com.springproject.FirstProject;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+
+    @GetMapping("/hello")
+    private String displayHello(){
+        return "Hello world";
+    }
+
 }
